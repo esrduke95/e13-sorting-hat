1369ce9c7f8d6e0fab358962929ed196a107eb84
Summer Duke <esrduke@gmail.com>
2020-07-30 19:09:31 -0500

    added html setup ft. some light js and css

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('what is up my dudes');
+//
+
+const onClickEvents = () => {
+  document.querySelector("#start-btn").addEventListener("click", makeForm);
+};
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML = textToPrint;
+};
+const makeForm = () => {
+  let domString = `<div id="form-wrapper">
+                        <h5>Enter First Year's Name</h5>
+                        <form class="form-inline" id="sorting-form">
+                            <label class="sr-only" for="inlineFormInputName2">STUDENT NAME</label>
+                            <input type="text" class="form-control mr-sm-2" id="inlineFormInputName2" placeholder="Harry Potter">    
+                            <button type="submit" class="btn btn-primary">Sort!</button>
+                        </form>
+                    </div>`;
+  printToDom("sorting-form", domString);
+  studentButton();
+};
+
+const init = () => {
+    makeForm();
+}
+
+init();
